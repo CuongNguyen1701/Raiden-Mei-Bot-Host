d44e4ed2319323a2b10dcbe3519af3b51c92fcaa
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2020-04-19 14:32:00 +0700

    fix prefix problem

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ client.on('ready', async () => {
 
 client.on('message', message => 
 {   
-
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).split(/ +/);
     const commandName = args.shift().toLowerCase();
     const command = message.client.commands.get(commandName)
