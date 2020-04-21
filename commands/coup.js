161104f8facfa0e3b2fb17f40f92c14db0780241
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2020-04-22 00:15:31 +0700

    Update coup.js

--- commands/coup.js
+++ commands/coup.js
@@ -3,11 +3,33 @@ const { currency } = require('../config.json');
 
 const { coupData } = require('../coup.json');
 
+const mongoose = require('mongoose');
+
+
+//CONNECT TO DATABASE
+mongoose.connect(process.env.mongoPass, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Database Connected'))
+    .catch(err => console.log(err));
+
+//MODELS
+const Data = require('../models/data.js');
 
 module.exports = {
     name: 'coup',
-    description: 'kiểm tra giá trị cổ phiếu',
+    description: 'kiểm tra giá trị cổ phiếu và số cổ phiếu trong kho',
     execute(client, message, args) {
-        message.channel.send('current coupon value: ' + coupData.coupValue + currency)
+        Data.findOne({
+            userID: message.author.id
+        }, (err, data) => {
+            if (err) console.log(err);
+            if (!data) { //check if user has no data on database
+                return message.reply('please use ' + prefix + 'create first to see your coupon');
+            }
+            else{
+                message.channel.send('your coupon: '+ data.coup +'. Current coupon value: ' + coupData.coupValue + currency);
+                
+            }
+        })
+
     }
 }
