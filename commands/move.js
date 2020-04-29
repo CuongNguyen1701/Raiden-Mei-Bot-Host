d92bddd12300f438a85b20eecb0bf81fada23273
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2020-04-29 20:30:20 +0700

    Update move.js

--- commands/move.js
+++ commands/move.js
@@ -5,6 +5,7 @@ const { prefix } = require('../config.json');
 const mongoose = require('mongoose');
 const fs = require ('fs');
 const rpgFiles = fs.readdirSync('../rpgfiles', (err, files) =>{
+    if(err) console.log(err)
     files.foreach(file => {
         console.log(file + 'loaded');
     })
