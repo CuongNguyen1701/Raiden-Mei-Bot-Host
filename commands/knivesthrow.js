d84e29e24362d830499698a5df739bca651a66e1
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2020-05-04 23:39:06 +0700

    Update knivesthrow.js

--- commands/knivesthrow.js
+++ commands/knivesthrow.js
@@ -23,7 +23,7 @@ module.exports = {
 	description: '[Ranger+]phi dao, đòn tầm xa và có thể crit (75 MP)',
 	cooldown: 60,
 	execute(client, message, args) {
-		if (args[0] == 'boss'|| !args[0]) {
+		if (args[0] == 'boss' || !args[0]) {
 			var user = new Object();
 			user.id = 1;
 		}
@@ -53,12 +53,12 @@ module.exports = {
 					return message.reply('user has not enter the map yet!');
 				}
 				var rangerType = ['ranger', 'ninja', 'vampire']
-                if (!rangerType.includes(author_rpgData.class)) {
+				if (!rangerType.includes(author_rpgData.class)) {
 					return message.reply("This is an exclusive move of " + rangerType + "!");
 				}
 				let range = 2;
 				let mpCost = 75;
-	
+
 
 				//both directions' distance is larger than 1
 				if ((Math.abs(author_rpgData.posY - user_rpgData.posY) > range) || (Math.abs(author_rpgData.posX - user_rpgData.posX) > range)) {
@@ -68,30 +68,30 @@ module.exports = {
 				if (author_rpgData.hp <= 0) return message.reply('you are already dead!');
 				if (user_rpgData.hp <= 0) return message.reply(user_rpgData.name + ' is already dead!');
 
-				let dmg = parseInt(Math.log(author_rpgData.atk) / Math.log(user_rpgData.def) * 300) + RandInt(100, 200);
+				let dmg = parseInt(Math.log(author_rpgData.atk) / Math.log(user_rpgData.def) * 350) + RandInt(100, 200);
 				if (author_rpgData.mp < mpCost) return message.reply("you don't have enough MP!");
-				switch (author_rpgData.class){
+				switch (author_rpgData.class) {
 					case rangerType[1]:
-						dmg = Math.round(dmg*1.4);
+						dmg = Math.round(dmg * 1.4);
 						break;
 					case rangerType[2]:
-						dmg = Math.round(dmg*1.1) + RandInt(50, 300);
-						
-						break;
-	
-                }
-                function CritRate(critRate) {
-                    let chance = RandInt(1, 100);
-                    if (chance <= critRate) dmg *= 2;
-                    crit = true;
+						dmg = Math.round(dmg * 1.1) + RandInt(50, 300);
 
-                }
-                CritRate(30);
+						break;
+
+				}
+				function CritRate(critRate) {
+					let chance = RandInt(1, 100);
+					if (chance <= critRate) dmg *= 2;
+					crit = true;
+
+				}
+				CritRate(30);
 				author_rpgData.mp -= mpCost;
 				user_rpgData.hp -= dmg;
 
-                if (crit) embed.setTitle('CRIT!! ' + author_rpgData.name + ' knives attack!');
-                else embed.setTitle(author_rpgData.name + ' knives attack!');
+				if (crit) embed.setTitle('CRIT!! ' + author_rpgData.name + ' knives attack!');
+				else embed.setTitle(author_rpgData.name + ' knives attack!');
 				embed.setDescription(author_rpgData.name + ' deal ' + dmg + ' damage to ' + user_rpgData.name + '!')
 				if (user_rpgData.hp <= 0) {
 					user_rpgData.hp = 0;
