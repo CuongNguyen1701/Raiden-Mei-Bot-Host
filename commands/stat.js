50c1991110ecb865b6becd64cf59ca1459f31231
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2020-06-23 16:26:42 +0700

    Update stat.js

--- commands/stat.js
+++ commands/stat.js
@@ -8,7 +8,6 @@ const createData = require('../triggers/createData');
 
 //CONNECT TO DATABASE
 mongoose.connect(process.env.mongoPass, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Database Connected'))
     .catch(err => console.log(err));
 //MODELS
 const data = require('../models/data.js');
@@ -45,7 +44,6 @@ module.exports = {
             if (!data) {
                 createData.execute(client, user);
             }
-            SaveData(data);
 
             embed.setTitle(`${data.name} 's stats`);
             embed.addField('Level: ', data.level, true);
