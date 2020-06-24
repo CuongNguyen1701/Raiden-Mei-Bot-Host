f27db7deb574c8808dc37bc5c285ac909cc99fc1
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2020-06-24 14:12:59 +0700

    update set

---
+++ commands/set.js
@@ -0,0 +1,92 @@
+const Discord = require('discord.js');
+const ms = require('parse-ms');
+
+const { currency, prefix, mongoPass } = require('../config.json');
+const role = require('../roles.json');
+const color = require('../color.json');
+
+const mongoose = require('mongoose');
+
+
+//CONNECT TO DATABASE
+mongoose.connect(process.env.mongoPass, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Database Connected'))
+    .catch(err => console.log(err));
+
+//MODELS
+const CalcData = require('../models/calcdata.js');
+const data = require('../models/data');
+module.exports = {
+    name: '',
+    description: '',
+    execute(client, message, args) {
+        let user = message.author;
+        function SaveData(data) { data.save().catch(err => console.log(err)); }
+        CalcData.findOne({}, (err, calcData) => {
+            if (!calcData) {
+                var newData = new CalcData({
+                    name: client.users.cache.get(user.id).username,
+                    userID: user.id,
+                    lv: 80,
+                    atk: 1000,
+                    crt: 100,
+                    tdm: 0,
+                    tdm_r: 0,
+                    phys: 0,
+                    phys_r: 0,
+                    cdm: 0,
+                    ele: 0,
+                    ele_r: 0,
+                })
+                SaveData(newData);
+            }
+            let data = newData || calcData;
+
+            if (args[1] < 0 || isNaN(args[1])) return message.reply(`please use a positive number`)
+            let correctInput = true;
+            switch (args[0]) {
+                case 'lv': case 'level':
+                    if (args[1] > 80) return message.reply(`please enter a whole number between 1 to 80 for valkyrie's level`)
+                    args[1] = Math.floor(args[1]);
+                    data.lv = args[1];
+                    break;
+                case 'atk': case 'attack':
+                    args[1] = Math.floor(args[1]);
+                    data.atk = args[1];
+                    break;
+                case 'crt':
+                    args[1] = Math.floor(args[1]);
+                    data.crt = args[1];
+                    break;
+                case 'tdm':
+                    data.tdm = args[1];
+                    break;
+                case 'tdm_r':
+                    data.tdm_r = args[1];
+                    break;
+                case 'phys':
+                    data.phys = args[1];
+                    break;
+                case 'phys_r':
+                    data.phys_r = args[1];
+                    break;
+                case 'cdm':
+                    data.cdm = args[1];
+                    break;
+                case 'ele':
+                    data.ele = args[1];
+                    break;
+                case 'ele_r':
+                    data.ele_r = args[1];
+                    break;
+                default:
+                    correctInput = false;
+                    message.reply(`please use the correct syntax!`);
+                    break;
+            }
+            if (correctInput) message.channel.send(`${args[0]} is set to ${args[1]}`);
+
+        })
+    }
+
+}
---
+++ models/calcdata.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const dataSchema = mongoose.Schema({
+    name: String,
+    userID: String,
+    lv: Number,
+    atk: Number,
+    crt: Number,
+    tdm: Number,
+    tdm_r: Number,
+    phys: Number,
+    phys_r: Number,
+    cdm: Number,
+    ele: Number,
+    ele_r: Number,
+
+
+})
+
+module.exports = mongoose.model('CalcData', dataSchema);
