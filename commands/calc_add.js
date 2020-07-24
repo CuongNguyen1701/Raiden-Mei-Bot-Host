b3131657c07bd753a8fc0e493b134c5983409bfd
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2020-07-24 12:55:14 +0700

    Update calc_add.js

--- commands/calc_add.js
+++ commands/calc_add.js
@@ -42,54 +42,52 @@ module.exports = {
             if (isNaN(args[1])) return message.reply(`please use a number`)
             let correctInput = true;
             var newValue;
+            let addition = Math.floor(args[1])
             switch (args[0]) {
                 case 'lv': case 'level':
-                    if (args[1] > 80) return message.reply(`please enter a whole number between 1 to 80 for valkyrie's level`)
-                    args[1] = Math.floor(args[1]);
-                    data.lv += args[1];
+                    if (addition > 80) return message.reply(`please enter a whole number between 1 to 80 for valkyrie's level`)
+                    data.lv = parseInt(data.lv) + addition;
                     if (data.lv > 80) data.lv = 80;
                     newValue = data.lv;
                     break;
                 case 'atk': case 'attack':
-                    args[1] = Math.floor(args[1]);
-                    data.atk += args[1];
+                    data.atk = parseInt(data.atk) + addition;
                     newValue = data.atk;
                     break;
                 case 'crt':
-                    args[1] = Math.floor(args[1]);
-                    data.crt += args[1];
+                    data.crt = parseInt(data.crt) + addition;
                     newValue = data.crt
                     break;
                 case 'tdm':
-                    data.tdm += args[1];
+                    data.tdm = parseInt(data.tdm) + addition;
                     newValue = data.tdm
                     break;
-                case 'tdm_r':
-                    data.tdm_r += args[1];
+                case 'tdm_r': case 'tdmr':
+                    data.tdm_r = parseInt(data.tdm_r) + addition;
                     newValue = data.tdm_r
                     break;
                 case 'phys':
-                    data.phys += args[1];
+                    data.phys = parseInt(data.phys) + addition;
                     newValue = data.phys
                     break;
-                case 'phys_r':
-                    data.phys_r += args[1];
+                case 'phys_r': case 'physr':
+                    data.phys_r = parseInt(data.phys_r) + addition;
                     newValue = data.phys_r
                     break;
-                case 'cdm':
-                    data.cdm += args[1];
+                case 'cdm': case 'crit_damage': case 'crit_dmg':
+                    data.cdm = parseInt(data.cdm) + addition;
                     newValue = data.cdm
                     break;
                 case 'ele':
-                    data.ele += args[1];
+                    data.ele = parseInt(data.ele) + addition;
                     newValue = data.ele
                     break;
-                case 'ele_r':
-                    data.ele_r += args[1];
+                case 'ele_r': case 'ele_r':
+                    data.ele_r = parseInt(data.ele_r) + addition;
                     newValue = data.ele_r
                     break;
                 case 'bonus_crit':
-                    data.bonus_crit += args[1];
+                    data.bonus_crit = parseInt(data.bonus_crit) + addition;
                     newValue = data.bonus_crit
                     break;
                 default:
