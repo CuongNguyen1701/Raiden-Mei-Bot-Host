b452aac8e435dc7e1eba3fc56d784e7b6c7681c1
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2020-08-04 21:57:43 +0700

    Update (luckynum)check.js

--- commands/(luckynum)check.js
+++ commands/(luckynum)check.js
@@ -17,32 +17,27 @@ module.exports = {
     execute(client, message, args) {
 
 
-        let embed = new Discord.MessageEmbed();
 
-        if (!args[0] || !Number.isInteger(Number(args[0]))) {
-            return message.reply(`please enter a whole number`)
-        } else {
-
-            Data.find({
-                checkable: true
-            }).sort([
-                ['guess', 'descending']
-            ]).exec((err, res) => {
-                if (err) console.log(err);
-                if (!res) return message.reply(`there's no guess yet`);
-                let msg = `guessed numbers: `
-                for (i = 0; i < res.length; i++) {
-                    if (i % 5 == 0) {
-                        msg += `\n ${res[i].guess}`
-                    } else {
-                        msg += `  ${res[i].guess}`
-                    }
+        Data.find({
+            checkable: true
+        }).sort([
+            ['guess', 'descending']
+        ]).exec((err, res) => {
+            if (err) console.log(err);
+            if (!res) return message.reply(`there's no guess yet`);
+            let msg = `guessed numbers: `
+            for (i = 0; i < res.length; i++) {
+                if (i % 5 == 0) {
+                    msg += `\n ${res[i].guess}`
+                } else {
+                    msg += `  ${res[i].guess}`
                 }
-                message.channel.send(msg);
-            })
+            }
+            message.channel.send(msg);
+        })
 
 
-        }
+
 
 
 
