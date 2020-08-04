81ccf0af564dfd6041dc4ab688b246643cda1236
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2020-08-04 22:52:58 +0700

    Update (luckynum)check.js

--- commands/(luckynum)check.js
+++ commands/(luckynum)check.js
@@ -16,7 +16,11 @@ module.exports = {
     description: 'check số chưa được đoán',
     execute(client, message, args) {
 
-
+        var max = 57;
+        var guessPool = new Array(max)
+        for (var i = 0; i < max; i++) {
+            guessPool[i] = (i + 1);
+        }
 
         Data.find({
             checkable: true
@@ -25,11 +29,13 @@ module.exports = {
         ]).exec((err, res) => {
             if (err) console.log(err);
             if (!res) return message.reply(`there's no guess yet`);
-            let msg = 'guessed numbers: '
-            for (i = 0; i < res.length; i++) {
-                if ((i % 5) == 0) msg += `\n ${res[i].guess}`
+            let msg = 'available numbers: '
+            let available = guessPool.filter(guessedNum => !res.includes(guessedNum));
 
-                else msg += `   ${res[i].guess}`
+            for (i = 0; i < res.length; i++) {
+                if ((i % 5) == 0) msg += `\n ${available[i]}`
+
+                else msg += `   ${available[i]}`
             }
             message.channel.send(msg);
         })
