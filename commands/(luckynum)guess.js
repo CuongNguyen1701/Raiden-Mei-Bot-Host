91d563f48a75b387bf835c6e12dda6a19e0bd30e
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2020-08-05 12:45:43 +0700

    Update (luckynum)guess.js

--- commands/(luckynum)guess.js
+++ commands/(luckynum)guess.js
@@ -24,41 +24,39 @@ module.exports = {
             id: 1
         }, (err, cooldown) => {
             if (err) console.log(err);
-            if (cooldown.guessDisabled)
-                return message.reply(`you cannot guess now!`);
-        })
+            if (cooldown.guessDisabled) return message.reply(`you cannot guess now!`)
+            if (!args[0] || !Number.isInteger(Number(args[0])) || args[0] < min || args[0] > max)
+                return message.reply(`please enter a whole number between ${min} and ${max}`);
+            Data.findOne({
+                userID: user.id
+            }, (err, data) => {
+                if (err) console.log(err);
+                if (!data) {
+                    Data.findOne({
+                        guess: args[0]
+                    }, (err, guessdata) => {
+                        if (err) console.log(err);
+                        if (!guessdata) {
+                            var newData = new Data({
+                                name: client.users.cache.get(user.id).username,
+                                userID: user.id,
+                                guess: args[0],
+                                checkable: true
+                            })
+                            SaveData(newData);
+                            return message.reply(`you guessed ${args[0]}!`);
+                        }
 
-        if (!args[0] || !Number.isInteger(Number(args[0])) || args[0] < min || args[0] > max)
-            return message.reply(`please enter a whole number between ${min} and ${max}`);
-
-        Data.findOne({
-            userID: user.id
-        }, (err, data) => {
-            if (err) console.log(err);
-            if (!data) {
-                Data.findOne({
-                    guess: args[0]
-                }, (err, guessdata) => {
-                    if (err) console.log(err);
-                    if (!guessdata) {
-                        var newData = new Data({
-                            name: client.users.cache.get(user.id).username,
-                            userID: user.id,
-                            guess: args[0],
-                            checkable: true
-                        })
-                        SaveData(newData);
-                        return message.reply(`you guessed ${args[0]}!`);
-                    }
-                    else {
                         return message.reply(`${args[0]} is already guessed by someone!`);
-                    }
-                })
-            }
-            else {
-                return message.reply(`you have already guessed ${data.guess}!`);
-            }
+
+                    })
+                }
+                else {
+                    return message.reply(`you have already guessed ${data.guess}!`);
+                }
+            })
         })
+
 
 
 
